a121259a90645161eb425f2b143b749fe896e423
Almagosi <almagosicode@gmail.com>
2024-06-15 12:08:19 +0200

    Añadido command handler

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Client } = require("discord.js");
 const client = new Client({intents:[3148800]});
 
+const fs = require('nodo:fs');
+const path = require('node:path');
+const { Collection, Event } = require("discord.js")
+
 client.config = require("./config.json");
 
 client
@@ -10,3 +14,25 @@ client
         client.user.setActivity(`DJ`);
     })
     .catch((error) => console.log(error));
+
+
+client.commands = new Collection();
+
+console.log(`DirName --> ${__dirname}`);
+const foldersPath = path.join(__dirname,'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for(const folder of commandFolders){
+    const commandsPath = path.join(foldersPath,folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for( const file of commandFiles){
+        const filePath = path.join(commandsPath,file);
+        const command = require(filePath);
+        if('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command);
+        }
+        else{
+            console.log(`[WARNING] El comando en ${filePath} depende de la propiedad "data" o "execute`);
+        }
+    }
+}
